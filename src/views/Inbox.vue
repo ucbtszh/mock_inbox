17af64f9fbac910be892985087a3acee9b186f16
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-11-05 23:18:50 +0000

    90% Outlook inbox visual sim

---
+++ src/views/Inbox.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-app id="inbox">
+    <v-toolbar height="50" color="rgb(0,120,212)" app>
+      <p id="outlook-sign">Outlook</p>
+      <v-spacer></v-spacer>
+      <v-icon dark>mdi-account</v-icon>
+    </v-toolbar>
+
+    <v-toolbar dense color="rgb(242,242,242)" flat>
+      <v-btn depressed><v-icon>mdi-reply</v-icon>&nbsp;Reply</v-btn>
+      <v-btn depressed><v-icon>mdi-arrow-right</v-icon>&nbsp;Forward</v-btn>
+      <v-btn depressed><v-icon>mdi-delete</v-icon>&nbsp;Delete</v-btn>
+      <v-btn depressed><v-icon>mdi-archive-outline</v-icon>&nbsp;Archive</v-btn>
+      <v-btn depressed><v-icon>mdi-block-helper</v-icon>&nbsp;Junk</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn depressed color="secondary">DONE</v-btn>
+    </v-toolbar>
+
+    <v-main>
+      <v-row>
+        <v-col cols="4">
+          <v-card style="height: 100vh; overflow: auto">
+            <v-card-title>Inbox</v-card-title>
+
+            <v-list>
+              <template v-for="(eml, index) in emls">
+                <v-list-item
+                  :key="index"
+                  @click="displayEml(index, eml.bodyURL)"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>{{ eml.fromName }}</v-list-item-title>
+
+                    <v-list-item-subtitle>
+                      <div style="float: left">
+                        {{ eml.subject.substr(0, 60) }}
+                      </div>
+                      <div style="float: right">{{ eml.time }}</div>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-list>
+          </v-card>
+        </v-col>
+
+        <v-col style="height: 100vh; overflow: auto">
+          <v-card v-for="(eml, index) in emls" :key="index">
+            <div
+              v-if="index === emlClickIndex"
+              style="background-color: 'yellow'"
+            >
+              <v-card-title
+                ><div class="subject">{{ eml.subject }}</div></v-card-title
+              >
+              <div class="initial">
+                {{ eml.fromName.substr(0, 1) }}
+              </div>
+              <div id="header" style="background-color: 'yellow'; margin-left:65px;">
+                <div class="from-name" id="eml_from_name">
+                  {{ eml.fromName }}&nbsp;&lt;{{ eml.fromEml }}&gt;
+                </div>
+                <div class="time" id="eml_timing">{{ eml.time }}</div>
+                <div class="to" id="eml_to">
+                  To:&nbsp;&nbsp;{{ eml.toEml }}<br />
+                  <p v-if="eml.CCeml.length > 0">CC: {{ eml.CCeml }}</p>
+                </div>
+              </div>
+              <iframe
+                :src="eml.bodyURL"
+                :height="eml.height + 26"
+                name="eml-msg"
+              />
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import emails from "../assets/stimuli_eml_full_shuffled.json";
+
+export default {
+  data: () => ({
+    emls: emails,
+    emlClickIndex: "",
+  }),
+  methods: {
+    displayEml(index, src) {
+      this.emlClickIndex = index;
+      document.getElementById("eml-msg").src = src;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#outlook-sign {
+  font-size: 18px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.v-btn {
+  text-transform: none !important;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif !important;
+}
+
+.subject {
+  font-size: 16pt;
+  font-weight: bold;
+}
+
+.initial {
+  height: 40px;
+  width: 40px;
+  background-color: indigo;
+  border-radius: 50%;
+  float: left;
+  font-size: 12pt;
+  color: white;
+  text-align: center;
+  line-height: 2.5em;
+  margin-left: 15px;
+}
+
+.from-name {
+  margin-right: 5px;
+}
+
+.from-eml {
+  line-height: 1.8em;
+}
+
+.time {
+  font-size: 9pt;
+  color: gray;
+  line-height: 1.8em;
+  margin-bottom: 3px;
+}
+
+.to {
+  font-size: 11pt;
+}
+
+iframe {
+  width: 95%;
+  border: 0;
+  display: block;
+  margin-left: 58px;
+  padding: 0;
+}
+</style>
