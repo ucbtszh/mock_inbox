d7796f7bd12023c205f857f01ec98ca3e1e4071a
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-11-06 22:04:56 +0000

    init firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import firebase from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEJIpYYSBk-S2iWKXgJNAxOpeBKiNgZ3M",
+  authDomain: "mock-inbox.firebaseapp.com",
+  projectId: "mock-inbox",
+  storageBucket: "mock-inbox.appspot.com",
+  messagingSenderId: "787393509996",
+  appId: "1:787393509996:web:0e91453e8c2f323bb1b649"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
