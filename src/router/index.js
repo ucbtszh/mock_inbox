37777d736a3d02100e8a530e19080f63f6e46d95
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-11-08 17:44:14 +0000

    require user ID for each view other than Consent

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,42 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Meta from 'vue-meta'
-import Consent from '../views/Consent.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Meta from "vue-meta";
+import Consent from "../views/Consent.vue";
+import { auth } from "../main"
 
-Vue.use(VueRouter)
-Vue.use(Meta)
+Vue.use(VueRouter);
+Vue.use(Meta);
 
 const routes = [
   {
-    path: '/',
-    name: 'Consent',
-    component: Consent
+    path: "/",
+    name: "Consent",
+    component: Consent,
   },
   {
-    path: '/inbox',
-    name: 'Inbox',
-    component: () => import(/* webpackChunkName: "inbox" */ '../views/Inbox.vue'),
+    path: "/inbox",
+    name: "Inbox",
+    component: () =>
+      import(/* webpackChunkName: "inbox" */ "../views/Inbox.vue"),
     meta: {
-      requiresAuth: true
-    }
-  }
-]
+      requiresAuth: true,
+    },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+  if ((requiresAuth && !Vue.prototype.$user) | (requiresAuth && !auth.currentUser)) {
+    next('/')
+  } else {
+    next();
+  }
+});
+
+export default router;
