00581467c0843e50b56e6690de3faa702f597117
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2021-11-12 17:55:55 +0000

    changed instructions to component

---
+++ src/components/InstructTxt.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    Imagine that you are Alex Carter, an executive director of a fictive company
+    called Anneon.<br /><br />
+
+    Your task in this study is to process Alex' e-mails as you would do as an
+    executive director.<br /><br />
+
+    You will see a simulated version of Alex Carter's Outlook inbox.<br />
+    It contains 47 e-mails that need to be handled adequately:<br /><br />
+
+    <img src="../assets/ss_inbox_control.png" width="100%" /><br /><br />
+
+    You need to select one of the e-mails and then any of the available options
+    that you find applicable. The options are similar to what you would
+    typically find in your Outlook e-mail inbox:<br /><br />
+    <ul>
+      <li><b>Reply</b>: allows you to reply to the sender of the e-mail</li>
+      <li>
+        <b>Forward</b>: allows you to forward the e-mail to your executive
+        assistant to take care of it
+      </li>
+      <li><b>Archive</b>: will move the e-mail to an archive</li>
+      <li><b>Delete</b>: will delete the e-mail</li>
+      <li>
+        <b>Junk</b>: will mark the e-mail as junk and move it to the junk folder
+      </li>
+    </ul>
+    <br />
+
+    When you are done processing all e-mails, click "DONE":<br /><br />
+    <img src="../assets/ss_done.png" width="100%" />
+  </div>
+</template>
