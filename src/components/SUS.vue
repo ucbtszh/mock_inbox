8b47ad6f81ab2605e2dc015096d95493ada1068e
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2022-01-05 19:37:38 +0000

    substitute NASA-TLX with SUS

---
+++ src/components/SUS.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    We would like to know how you experienced using the inbox.<br />
+    Please indicate how much you agree with each of the below statements.<br /><br />
+    <v-form v-model="isValid" ref="form">
+      <Scale
+        v-for="(item, index) in items"
+        :key="'item' + index"
+        :item="item.item"
+        @answered="saveResponse"
+      />
+      <v-btn
+        color="primary"
+        elevation="3"
+        :disabled="!isValid"
+        @click="validate()"
+      >
+        NEXT
+      </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import items from "../assets/SUS.json";
+import Scale from "./Likert.vue";
+import db from "../utils/firestore";
+
+export default {
+  components: {
+    Scale,
+  },
+  mixins: [db],
+  data() {
+    return {
+      items: items,
+      responses: {},
+      isValid: true,
+    };
+  },
+  methods: {
+    saveResponse(event) {
+      this.responses[event[0]] = event[1];
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        try {
+          this.writeResponseData(this.$user, "SUS", this.responses);
+          this.$emit("done");
+        } catch (error) {
+          alert("Could not submit data.", error);
+        }
+      } else return;
+    },
+  },
+};
+</script>
--- src/views/Surveys.vue
+++ src/views/Surveys.vue
@@ -1,11 +1,8 @@
 <template>
   <div id="surveys">
-    <div v-show="showNASA">
-      <NASA @done="showDemos = true; showNASA = false; scrollUp()" />
+    <div v-show="showSUS">
+      <SUS @done="showDemos = true; showSUS = false; scrollUp()" />
     </div>
-    <!-- <div v-show="showUsab">
-      <Usab />
-    </div> -->
     <div v-show="showDemos">
       <Demos />
     </div>
@@ -14,20 +11,17 @@
 
 <script>
 import Demos from "../components/Demos.vue";
-// import Usab from "../components/Usability.vue";
-import NASA from "../components/NASA-TLX.vue";
+import SUS from "../components/SUS.vue";
 
 export default {
   components: {
     Demos,
-    // Usab,
-    NASA,
+    SUS,
   },
   data() {
     return {
-      showNASA: true,
-      showUsab: false,
-      showDemos: false,
+      showSUS: true,
+      showDemos: false
     };
   },
   methods: {
