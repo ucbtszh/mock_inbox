c27578889387b8a0208037716e6b63511e1e737f
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2022-01-11 19:04:35 +0100

    more detailed instructions

--- src/components/InstructTxt.vue
+++ src/components/InstructTxt.vue
@@ -6,27 +6,37 @@
     Your task in this study is to process Alex' e-mails as you would do as an
     executive director.<br /><br />
 
-    You will see a simulated version of Alex Carter's Outlook inbox.<br />
-    It contains 47 e-mails that need to be handled adequately:<br /><br />
+    You will see a snapshot of Alex Carter's inbox.<br />
+    It contains 47 e-mails that need to be processed adequately:<br /><br />
 
     <img src="../assets/ss_inbox_control.png" width="100%" /><br /><br />
 
-    You need to select one of the e-mails and then any of the available options
-    that you find applicable. The options are similar to what you would
-    typically find in your Outlook e-mail inbox:<br /><br />
+    The inbox has similar functionalities as what you typically would find in an
+    actual inbox.<br />
+    To process an e-mail, click on an e-mail in the list on the left.<br />
+    The full e-mail will be displayed on the right. <br /><br />
+
+    All available actions that you can take with the selected e-mail are displayed in the bar above
+    the e-mails.<br /><br />
+
     <ul>
-      <li><b>Reply</b>: allows you to reply to the sender of the e-mail</li>
+      <li><b>Reply</b>: allows you to reply to the sender of the e-mail.</li>
       <li>
-        <b>Forward</b>: allows you to forward the e-mail to your executive
-        assistant to take care of it
+        <b>Forward</b>: forward the e-mail to your executive assistant (EA), so
+        your EA can take care of it. You do not need to type an extra message
+        for this action.
       </li>
-      <li><b>Archive</b>: will move the e-mail to an archive</li>
-      <li><b>Delete</b>: will delete the e-mail</li>
+      <li><b>Archive</b>: move the e-mail to an archive.</li>
+      <li><b>Delete</b>: delete the e-mail.</li>
       <li>
-        <b>Junk</b>: will report the e-mail as junk and move it to a junk archive
+        <b>Junk</b>: report the e-mail as junk and move it to a junk
+        archive.
       </li>
     </ul>
     <br />
+    Your task is to choose what you think is the best applicable action for each
+    e-mail.<br />
+    Please note that this task should not take more than 30-60 minutes.<br /><br />
 
     When you are done processing all e-mails, click "DONE":<br /><br />
     <img src="../assets/ss_done.png" width="100%" />
