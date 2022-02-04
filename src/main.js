2385f77cfe5215dab785968471ef53491fa52fef
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2022-02-04 15:36:04 +0000

    config realtime db url

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAEJIpYYSBk-S2iWKXgJNAxOpeBKiNgZ3M",
   authDomain: "mock-inbox.firebaseapp.com",
   projectId: "mock-inbox",
+  databaseURL: "https://mock-inbox-default-rtdb.europe-west1.firebasedatabase.app/",
   storageBucket: "mock-inbox.appspot.com",
   messagingSenderId: "787393509996",
   appId: "1:787393509996:web:0e91453e8c2f323bb1b649",
