7f4a0a8a508997cbf51db23e45481ba9bf5df3ba
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2022-02-04 16:36:59 +0000

    use push instead of set for rtdb writes

--- src/utils/rtdb.js
+++ src/utils/rtdb.js
@@ -1,11 +1,11 @@
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, push } from "firebase/database";
 
 const db = getDatabase();
 
 export default {
     methods: {
         writeEvent(uid, event_class, event_data) {
-            set(ref(db, 'users/' + uid + '/' + event_class), { event_data })
+            push(ref(db, 'users/' + uid + '/' + event_class), { event_data })
         }
     }
 }
