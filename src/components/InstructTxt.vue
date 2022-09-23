0a93c6ab89fd0bf85924d43b623cf5a193ac22ae
ucbtszh <72318704+ucbtszh@users.noreply.github.com>
2022-09-23 12:16:57 +0100

    updated instructions

--- src/components/InstructTxt.vue
+++ src/components/InstructTxt.vue
@@ -1,53 +1,71 @@
 <template>
-  <div>
-    In this study, you will take the role of a chief executive named
-    <b>Alex Carter</b>, who works at a petrochemical company called
-    <b>Anneon</b>.<br /><br />
+  <swiper>
+    <swiper-slide>
+      In this study, you will take the role of a chief executive named
+      <b>Alex Carter</b>, who works at a petrochemical company called
+      <b>Anneon</b>.<br /><br />
 
-    As a top executive, you are responsible for making strategic and financial
-    decisions to keep the business running as profitably as possible.
-    <br /><br />
+      As a top executive, you are responsible for making strategic and financial
+      decisions to keep the business running as profitably as possible.
+      <br /><br />
 
-    These decisions can range from foreign investment deals, finding new
-    business partners, to changing local business operations.<br /><br />
+      These decisions can range from foreign investment deals, finding new
+      business partners, to changing local business operations.<br /><br />
+    </swiper-slide>
+    <swiper-slide>
+      Your task is to process e-mails that you received as Alex Carter.<br />
+      To do so, you will see a mock inbox like this:<br /><br />
 
-    Your task is to process e-mails that you received as Alex Carter.<br />
-    To do so, you will see a mock inbox like this:<br /><br />
+      <img src="../assets/ss_inbox.png" width="100%" /><br /><br />
 
-    <img src="../assets/ss_inbox_control.png" width="100%" /><br /><br />
+      Assume that all e-mails are unread.<br />
+    </swiper-slide>
+    <swiper-slide>
+      The inbox has similar functionalities as what you typically would find in
+      an actual e-mail inbox.<br /><br />
 
-    Assume that all e-mails are unread.<br/>
-    
-    The inbox has similar functionalities as what you typically would find in an
-    actual e-mail inbox.<br /><br />
-    
-    You process an e-mail by clicking on an e-mail in the list on the left.<br />
-    The full e-mail will then be displayed on the right. <br />
-    You then <b>choose one of the available actions</b> for the selected e-mail
-    from the bar displayed above the e-mails.<br /><br />
+      You process an e-mail by clicking on an e-mail in the list on the left.<br />
+      The full e-mail will then be displayed on the right. <br />
+      You then <b>choose one of the available actions</b> for the selected
+      e-mail from the bar displayed above the e-mails.<br /><br />
 
-    <ul>
-      <li><b>Reply</b>: allows you to reply to the sender of the e-mail.</li>
-      <li>
-        <b>Forward</b>: forward the e-mail to your executive assistant (EA), so
-        the EA can take care of it. You do not need to type an extra message for
-        this action.
-      </li>
-      <li><b>Archive</b>: move the e-mail to an archive.</li>
-      <li><b>Delete</b>: delete the e-mail.</li>
-      <li>
-        <b>Junk</b>: report the e-mail as junk and move it to the junk folder.
-      </li>
-    </ul>
-    <br />
-    Process the e-mails as you would normally do, with your role as an executive
-    in mind.<br /><br />
-    You are free to use any text formatting when writing replies.<br/><br/>
-    
-    Note that all links in e-mails have
-    been disabled and that this task should not take more than 30-45 minutes.<br /><br />
+      <ul>
+        <li><b>Reply</b>: allows you to reply to the sender of the e-mail.</li>
+        <li>
+          <b>Forward</b>: forward the e-mail to your executive assistant (EA),
+          so the EA can take care of it. You do not need to type an extra
+          message for this action.
+        </li>
+        <li><b>Archive</b>: move the e-mail to an archive.</li>
+        <li><b>Delete</b>: delete the e-mail.</li>
+        <li>
+          <b>Junk</b>: report the e-mail as junk and move it to the junk folder.
+        </li>
+      </ul>
+    </swiper-slide>
+    <swiper-slide>
+      Process the e-mails as you would normally do, with your role as an
+      executive in mind.<br /><br />
+      You are free to use any text formatting when writing replies.<br /><br />
 
-    When you are done processing all e-mails, click "DONE":<br /><br />
-    <img src="../assets/ss_done.png" width="100%" />
-  </div>
+      Note that all links in e-mails have been disabled and that this task
+      should not take more than 30-45 minutes.<br /><br />
+
+      When you are done processing all e-mails, click "DONE":<br /><br />
+      <img src="../assets/ss_done.png" width="100%" />
+    </swiper-slide>
+  </swiper>
 </template>
+
+<script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+export default {
+  title: "Instructions",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+};
+</script>
