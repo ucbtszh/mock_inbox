cc781be85048e1eefe82d475cdd9c32c2e22e029
Sumair <sumairijazhashmi@gmail.com>
2023-09-02 14:57:58 +0500

    changed instructions

--- src/components/InstructTxt.vue
+++ src/components/InstructTxt.vue
@@ -1,8 +1,43 @@
 <template>
   <div>
-    Imagine that you are a renowned professor named Alex Carter in the field of health informatics, working at University of London (UOL).<br /><br />
-    In this study, you will see a mock inbox with e-mails adapted from actual e-mails received by academics.<br/>
-    Your task is to process these e-mails as if you received them in your role of Prof. Carter.<br /><br/>
+    In this exercise, you will be asked to prioritize various tasks, initiate actions, process 
+    information, make decisions, and coordinate deadlines in a meaningful way. You will 
+    be assessed on how you analyze information, process it organizationally and take
+    measures and make decisions within a given time frame.<br /><br/>
+
+    Please put yourself in the following situation: <br />  <br />
+
+    PTCL (Pakistan Telecommunication Company Ltd.) has established a new division in
+    Lahore in April 2022 to build the 5G network in Pakistan. You are A. Durrani (first
+    name is not specified for gender neutrality) and you are the head of this department. 
+    You oversee the organization and implementation of all necessary processes and
+    you are therefore responsible for the success of the project. <br />  <br />
+
+    Please refer to the organogram below for the organizational structure of
+    the employees relevant to you. <br/> <br/> 
+
+    <img src="../assets/organogram.png" width="100%"/> <br/> <br/>
+
+
+    Today is Monday, May 4 2023. You were on Eid vacation last week and did not check
+    your mails. You have already arrived at the office at 7:00 a.m. to complete the tasks
+    at hand. You have <strong>40 minutes</strong> to process the mails you have received, as you will no
+    longer be in the office from 7.40 a.m. onwards, because you have a number of other
+    appointments. You will not be able to reach any of your co-workers or business
+    partners, as the official start of work is not until 8:00. However, you can leave written
+    messages for your secretary, Ms. Amin. <br/> <br/>
+
+    The mails in your inbox are numbered to make it easier to keep them apart. The
+    order in which you process them is entirely up to you. <strong>Please process all mails!
+    Make a note of what your actions would be for each mail. During the
+    discussion of the results, we would like to know how you evaluate the mails in
+    detail, what conclusions you draw and what measures you take.</strong> <br /> <br/>
+
+    Note: In order to carry out the exercise, some restrictions must be made. There will
+    be no opportunity for consultation. <strong> The processing time is limited to 40 minutes.
+    You will be able to handle all the problem cases on your own. </strong> The processing is
+    possible without detailed expert knowledge.
+
 
     <img src="../assets/ss_inbox_control.png" width="100%" /><br /><br />
 
